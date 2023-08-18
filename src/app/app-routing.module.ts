f62c282dab7267121def712c736aa2cf870690d2
VigneshD-7788 <grtvignesh7788@gmail.com>
2023-08-18 16:10:47 +0530

    Routing and navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
+import { ExpenseEntryListComponent } from './expense-entry-list/expense-entry-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'expense-entry', component: ExpenseEntryComponent },
+  { path: 'expense-entry-list', component: ExpenseEntryListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
